4b5f7959ac708a0719c007980c0f28debab4479d
Damian Walker <dmanwalk3825@hotmail.com>
2021-09-03 09:00:56 -0500

    first commit, added tasks, added emit functions to pass data up components

---
+++ vue-crash2021/src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color ="green"></Button>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props:{
+        title:{
+            type: String,//declare prop data type
+            default:"Test",//define default value if no props are passed
+        }//pass the prop as an object
+    },
+    components:{
+        Button
+    }
+    
+    
+}
+</script>
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
---
+++ vue-crash2021/src/components/Task.vue
@@ -0,0 +1,45 @@
+<template>
+<div :class="[task.reminder ? 'reminder' : '', 'task']">  <!-- If task. reminder is true, add the reminder class, else add no class to div and always apply the task class-->
+<h3>
+    {{task.text}}
+    <i @click="onDelete(task.id)" class="fas fa-times"></i>
+</h3>
+<p>{{task.day}}</p>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props:{
+        task: Object,
+    },
+    methods:{
+        onDelete(id){
+            this.$emit('delete-task', id) ///emitting a custom callback function delete-task and passing the id as a param up to tasks
+        },
+    }
+}
+
+</script>
+
+<style scoped>
+.fas{
+    color: red;
+}
+.task{
+    background: #f3f3f3;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+.task h3{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.task.reminder{
+    border-left: 5px solid green;
+}
+</style>
